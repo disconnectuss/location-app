b72e74d2445a37ed5692e3d6f9b809012faebc06
Fatma A <fka201520@gmail.com>
2024-09-22 14:23:07 +0300

    layout updated with improvements

--- src/app/utils/getLoc.tsx
+++ src/app/utils/getLoc.tsx
@@ -1,16 +1,19 @@
-const getLoc = () => {
-  let currentLoc: [number, number] = [39.340544, 35.310927];
-
-  navigator.geolocation.getCurrentPosition(
-    (params) => {
-      currentLoc = [params.coords.latitude, params.coords.longitude];
-    },
-    () => {
-      console.log("Default location updated successfully");
-    }
-  );
-
-  return currentLoc;
+const getLoc = (): Promise<[number, number]> => {
+  return new Promise((resolve, reject) => {
+    let currentLoc: [number, number] = [39.340544, 35.310927];
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        currentLoc = [position.coords.latitude, position.coords.longitude];
+        resolve(currentLoc);
+      },
+      (error) => {
+        console.error(
+          "Error fetching geolocation, using default location:",
+          error
+        );
+        resolve(currentLoc);
+      }
+    );
+  });
 };
-
 export default getLoc;
