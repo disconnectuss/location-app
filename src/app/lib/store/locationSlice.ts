4f2581283dbf0e8aba5bc14212c881e0444b75ab
Fatma A <fka201520@gmail.com>
2024-09-25 13:42:54 +0300

    bugs fixed

--- src/app/lib/store/locationSlice.ts
+++ src/app/lib/store/locationSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
-import { Location, LocationState } from "@/utils/types";
+import { Location, LocationState } from "@/utils/types/types";
 const initialState: LocationState = {
   locations: [],
 };
